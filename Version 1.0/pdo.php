ed7145e68f5224eb7edaec0126193884fbdca05d
Pritesh Sahani <priteshsahani24@gmail.com>
2019-07-21 01:16:25 +0530

    Update pdo.php
    The database name, username and password was typed incorrectly.

--- Version 1.0/pdo.php
+++ Version 1.0/pdo.php
@@ -1,5 +1,7 @@
-<?php
+<?php            //Here connection with the database is made.
+
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=project1', 'pritesh', '123');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+?>
 
