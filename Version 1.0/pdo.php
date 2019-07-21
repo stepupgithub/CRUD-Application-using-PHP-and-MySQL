a3522677d9b515672ccad6454c44241d061f4e8d
Pritesh Sahani <priteshsahani24@gmail.com>
2019-07-21 12:37:48 +0530

    Update pdo.php

--- Version 1.0/pdo.php
+++ Version 1.0/pdo.php
@@ -1,6 +1,6 @@
 <?php            //Here connection with the database is made.
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=project1', 'pritesh', '123');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'fred', 'zap');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
